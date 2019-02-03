ae81245deb39fbfff9d903be50b06d2d7c5a3dd3
sarziv <saruzivila12@gmail.com>
2019-02-03 16:32:45 +0200

    ternary op

--- Basics/ternaryOperation.php
+++ Basics/ternaryOperation.php
@@ -1,12 +1,15 @@
 <?php
 //Ternary operation
-$a = 4;
+$a = 7;
 $b = ($a == 8) ?  "Yes its 8".PHP_EOL :  "NOT 8".PHP_EOL;
 echo $b;
 
 //is the same as
 if($a == 8 ){
-    echo "Yes its 8";
+    echo "Yes its 8\n";
 }else{
-    echo "NOT 8";
+    echo "NOT 8\n";
 }
+//if - elseif -  else
+$result = $a == 5 ? "Yes an 5\n": ( $a  == 8 ? "It's a 8 \n" : "No idea what is it.");
+echo $result;
