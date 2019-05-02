7c0403f226e44983303024e41eb81ee36367bdbc
sarziv <saruzivila12@gmail.com>
2019-05-02 18:13:57 +0300

    small change

--- Basics/basic.php
+++ Basics/basic.php
@@ -1,1 +1,15 @@
 <?php
+//test current - end - prev
+function fibonacci(array $num)
+{
+
+       $x = current($num); /*0*/
+       $y = end($num); /*1*/
+
+       $fib=($x+$y);
+       array_push($num,$fib);
+       print_r($num);
+       fibonacci($num);
+
+}
+fibonacci(array(0,1));
